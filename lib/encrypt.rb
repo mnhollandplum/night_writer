466c76fd331a3d0026fef63c6b08c51037503ff2
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-07 14:11:37 -0600

    adds test and method to convert individual chars into braile chars

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,21 @@
+require './lib/braile_dict.rb'
 class Encrypt
 
   attr_reader :to_be_translated_to_braile
 
   def initialize(to_be_translated_to_braile)
     @to_be_translated_to_braile = to_be_translated_to_braile
+    @dict = BraileDict.new
   end
 
   def take_input_convert_to_array
     @to_be_translated_to_braile.chars
   end
 
+  def converts_per_chars
+  single_chars = take_input_convert_to_array
+  single_chars.map do |chars|
+    @dict.english_to_braile[chars]
+  end
+  end
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -15,4 +15,11 @@ class EncryptTest < Minitest::Test
     result = encryptor.take_input_convert_to_array
     assert_equal ["h", "e", "l", "l", "o"], result
   end
+
+  def test_it_can_convert_braile_chars
+    encryptor = Encrypt.new("hello")
+    bd = BraileDict.new
+    result = encryptor.converts_per_chars
+    assert_equal ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00."], result
+  end
 end
