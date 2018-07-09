539e8c86bbd61960692d97460e72a1f61ade484c
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-09 11:04:14 -0600

    updates braile dict, encrypt file and test

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -16,10 +16,18 @@ class EncryptTest < Minitest::Test
     assert_equal ["h", "e", "l", "l", "o"], result
   end
 
-  def test_it_can_convert_braile_chars
+  def test_it_can_convert_chars
     encryptor = Encrypt.new("hello")
     bd = BraileDict.new
     result = encryptor.converts_per_chars
-    assert_equal ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00."], result
+    assert_equal [["0.","00",".."], ["0.",".0",".."], ["0.","0.","0."], ["0.","0.","0."], ["0.",".0","0."]], result
+  end
+
+  def test_it_can_split_a_single_char_into_rows
+    encryptor = Encrypt.new("a")
+    bd = BraileDict.new
+    expected = "0.\n..\n.."
+    actual = encryptor
+
   end
 end
