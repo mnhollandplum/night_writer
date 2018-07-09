d107794c03402e74bc8ba35e05af02532a24de25
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-09 11:53:19 -0600

    adds test and method for splitting braile into three rows

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -18,16 +18,35 @@ class EncryptTest < Minitest::Test
 
   def test_it_can_convert_chars
     encryptor = Encrypt.new("hello")
-    bd = BraileDict.new
     result = encryptor.converts_per_chars
     assert_equal [["0.","00",".."], ["0.",".0",".."], ["0.","0.","0."], ["0.","0.","0."], ["0.",".0","0."]], result
   end
 
-  def test_it_can_split_a_single_char_into_rows
+  def test_it_can_take_first_row_chars
     encryptor = Encrypt.new("a")
-    bd = BraileDict.new
-    expected = "0.\n..\n.."
-    actual = encryptor
+    expected = "0."
+    actual = encryptor.dict.english_to_braile["a"][0]
+    assert_equal expected, actual
 
   end
+
+  def test_it_can_make_rows
+    encryptor = Encrypt.new("a")
+    encryptor.converts_per_chars
+    encryptor.splits_rows
+    actual = encryptor.row_1
+    expected = ["0."]
+    assert_equal expected, actual
+    actual = encryptor.row_2
+    expected = [".."]
+    assert_equal expected, actual
+    actual = encryptor.row_3
+    expected = [".."]
+    assert_equal expected, actual
+  end
+
+
+
+
+
 end
