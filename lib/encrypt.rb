3a112012935437300c46eac24317fb107aaf0ca0
Pat <pat.mccartney2@gmail.com>
2018-07-11 16:18:52 -0600

    starting testing on decrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,10 +7,6 @@ class Encrypt
               :row_1,
               :row_2,
               :row_3,
-              :row_4,
-              :row_5,
-              :row_6,
-              :excess
 
 
   def initialize
